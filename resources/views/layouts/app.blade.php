a15b6444f047d651505e14c2ee22291daf60585c
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-20 15:39:34 +0200

    Add some links and spacing to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,17 @@
 
     <body>
         <!-- Website header -->
-        <header>
-            <nav>
-                <a href="/login">Login</a>
-                <a href="/register">Register</a>
+        <header id="site-header" class="p-4">
+            <nav class="d-flex justify-content-between">
+                <div class="links">
+                    <a class="mr-3" href="/">Home</a>
+                    <a href="/posts">Posts</a>
+                </div>
+
+                <div class="auth">
+                    <a class="mr-3" href="/login">Login</a>
+                    <a href="/register">Register</a>
+                </div>
             </nav>
         </header>
 
@@ -30,7 +37,7 @@
         </main>
 
         <!-- Website footer -->
-        <footer>
+        <footer id="site-footer">
 
         </footer>
 
