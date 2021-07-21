ee783eeee36d3a69f54c2923b6ef342783200a8f
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-22 00:12:21 +0200

    Add admin.posts.show view

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,14 @@
+@extends("layouts.admin")
+
+@section("content")
+    <div class="container">
+        <h1 class="font-weight-bold">{{ $post->title }}</h1>
+        
+        <div>{{ $post->body }}</div>
+        
+        <div class="author d-flex mt-5">
+            <div class="label font-weight-bold">Author:</div>
+            <div class="author-name ml-2">{{ $post->author }}</div>
+        </div>
+    </div>
+@endsection
