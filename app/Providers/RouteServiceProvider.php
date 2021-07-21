a8bf6750e746b7d30108cea2684b99a562155f58
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-22 00:26:50 +0200

    Change HOME constant in RouteServiceProvider (unsure)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/admin'; // Is this right?
 
     /**
      * Define your route model bindings, pattern filters, etc.
