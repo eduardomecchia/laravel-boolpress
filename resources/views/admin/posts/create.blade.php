944e70a7ebc7a9fd1200c283f3fc7d086d7c306a
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-22 11:44:18 +0200

    Add Post properties to $fillable and validation errors to frontend

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,6 +4,16 @@
 <div class="container">
     <h1>Create a post</h1>
 
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul class="m-0">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form class="d-flex flex-column" action="{{ route("admin.posts.store") }}" method="POST">
         @csrf
     
