b1339af8e5ebbdd57e9aacf37038c605d8fdeed1
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-24 16:55:21 +0200

    Add form to guest.contact view and fix some indentation problems

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,43 +1,43 @@
 @extends("layouts.admin")
 
 @section("content")
-<div class="container">
-    <h1>Create a post</h1>
+    <div class="container">
+        <h1>Create a post</h1>
 
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <ul class="m-0">
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul class="m-0">
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <form class="d-flex flex-column" action="{{ route("admin.posts.store") }}" method="POST" enctype="multipart/form-data">
-        @csrf
-    
-        <div class="post-title d-flex flex-column mb-4">
-            <label class="mr-3" for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ old("title") }}">
-        </div>
+        <form class="d-flex flex-column" action="{{ route("admin.posts.store") }}" method="POST" enctype="multipart/form-data">
+            @csrf
+        
+            <div class="post-title d-flex flex-column mb-4">
+                <label class="mr-3" for="title">Title</label>
+                <input type="text" name="title" id="title" value="{{ old("title") }}">
+            </div>
 
-        <div class="post-image d-flex flex-column mb-4">
-            <label class="mr-3" for="image">Image</label>
-            <input type="file" name="image" id="image">
-        </div>
+            <div class="post-image d-flex flex-column mb-4">
+                <label class="mr-3" for="image">Image</label>
+                <input type="file" name="image" id="image">
+            </div>
 
-        <div class="post-body d-flex flex-column mb-4">
-            <label class="mr-3" for="title">Body</label>
-            <input type="text" name="body" id="body" value="{{ old("body") }}">
-        </div>
+            <div class="post-body d-flex flex-column mb-4">
+                <label class="mr-3" for="title">Body</label>
+                <input type="text" name="body" id="body" value="{{ old("body") }}">
+            </div>
 
-        <div class="post-author d-flex flex-column mb-4">
-            <label class="mr-3" for="title">Author</label>
-            <input type="text" name="author" id="author" value="{{ old("author") }}">
-        </div>
+            <div class="post-author d-flex flex-column mb-4">
+                <label class="mr-3" for="title">Author</label>
+                <input type="text" name="author" id="author" value="{{ old("author") }}">
+            </div>
 
-        <button type="submit" class="btn btn-primary my-4">Submit post</button>
-    </form>
-</div>
+            <button type="submit" class="btn btn-primary my-4">Submit post</button>
+        </form>
+    </div>
 @endsection
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,44 +1,44 @@
 @extends("layouts.admin")
 
 @section("content")
-<div class="container">
-    <h1>Edit a post</h1>
+    <div class="container">
+        <h1>Edit a post</h1>
 
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <ul class="m-0">
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul class="m-0">
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <form class="d-flex flex-column" action="{{ route("admin.posts.update", $post->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method("PUT")
-    
-        <div class="post-title d-flex flex-column mb-4">
-            <label class="mr-3" for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ $post->title }}">
-        </div>
+        <form class="d-flex flex-column" action="{{ route("admin.posts.update", $post->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method("PUT")
+        
+            <div class="post-title d-flex flex-column mb-4">
+                <label class="mr-3" for="title">Title</label>
+                <input type="text" name="title" id="title" value="{{ $post->title }}">
+            </div>
 
-        <div class="post-image d-flex flex-column mb-4">
-            <label class="mr-3" for="image">Image</label>
-            <input type="file" name="image" id="image">
-        </div>
+            <div class="post-image d-flex flex-column mb-4">
+                <label class="mr-3" for="image">Image</label>
+                <input type="file" name="image" id="image">
+            </div>
 
-        <div class="post-body d-flex flex-column mb-4">
-            <label class="mr-3" for="title">Body</label>
-            <input type="text" name="body" id="body" value="{{ $post->body }}">
-        </div>
+            <div class="post-body d-flex flex-column mb-4">
+                <label class="mr-3" for="title">Body</label>
+                <input type="text" name="body" id="body" value="{{ $post->body }}">
+            </div>
 
-        <div class="post-author d-flex flex-column mb-4">
-            <label class="mr-3" for="title">Author</label>
-            <input type="text" name="author" id="author" value="{{ $post->author }}">
-        </div>
+            <div class="post-author d-flex flex-column mb-4">
+                <label class="mr-3" for="title">Author</label>
+                <input type="text" name="author" id="author" value="{{ $post->author }}">
+            </div>
 
-        <button type="submit" class="btn btn-primary my-4">Edit post</button>
-    </form>
-</div>
+            <button type="submit" class="btn btn-primary my-4">Edit post</button>
+        </form>
+    </div>
 @endsection
