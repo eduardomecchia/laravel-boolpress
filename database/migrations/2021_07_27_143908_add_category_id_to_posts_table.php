b8b06faf79fd26f7439be3b75bf08b945351cff3
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-27 17:01:25 +0200

    Complete new migration

--- database/migrations/2021_07_27_143908_add_category_id_to_posts_table.php
+++ database/migrations/2021_07_27_143908_add_category_id_to_posts_table.php
@@ -14,7 +14,8 @@ class AddCategoryIdToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger("category_id")->nullable()->after("id");
+            $table->foreign("category_id")->references("id")->on("categories")->onDelete("set null");
         });
     }
 
@@ -26,7 +27,8 @@ class AddCategoryIdToPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropForeign("posts_category_id_foreign");
+            $table->dropColumn("category_id");
         });
     }
 }
