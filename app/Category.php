5c73ac799b0c0a1b765f5aadfdea6a2bde4df517
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-27 16:37:10 +0200

    Document posts method in Category model

--- app/Category.php
+++ app/Category.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    /**
+     * Get all of the posts for the Category
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function posts()
     {
-        return $this->hasMany("App\Post");
+        return $this->hasMany(Post::class);
     }
 }
