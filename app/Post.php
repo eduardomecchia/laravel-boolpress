69b25df5a9c3fbf4b89106936e77d3bd3410a24f
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-27 16:34:28 +0200

    Document category method in Post model

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,11 @@ class Post extends Model
 {
     protected $fillable = ["title", "body", "author", "image"];
 
+    /**
+     * Get the category that owns the Post
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category()
     {
         return $this->belongsTo(Category::class);
