93e16e8775d9d5f8fe3aa9913454f00db8ccca7b
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-28 15:00:40 +0200

    Add posts method to Tag model

--- app/Tag.php
+++ app/Tag.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Tag extends Model
 {
-    //
+    /**
+     * The posts that belong to the tag.
+     */
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
