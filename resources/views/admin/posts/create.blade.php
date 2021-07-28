93d728329d4baa343392181260e539cfa0f2404e
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-28 15:09:37 +0200

    Add comments to create form and edit form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -8,29 +8,35 @@
 
         @include("partials.errors")
 
+        <!-- Form for creating a post -->
         <form class="d-flex flex-column" action="{{ route("admin.posts.store") }}" method="POST" enctype="multipart/form-data">
             @csrf
         
+            <!-- Post title -->
             <div class="post-title d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Title</label>
                 <input type="text" name="title" id="title" value="{{ old("title") }}">
             </div>
 
+            <!-- Post image -->
             <div class="post-image d-flex flex-column mb-4">
                 <label class="mr-3" for="image">Image</label>
                 <input type="file" name="image" id="image">
             </div>
 
+            <!-- Post body -->
             <div class="post-body d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Body</label>
                 <input type="text" name="body" id="body" value="{{ old("body") }}">
             </div>
 
+            <!-- Post author -->
             <div class="post-author d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Author</label>
                 <input type="text" name="author" id="author" value="{{ old("author") }}">
             </div>
 
+            <!-- Post category -->
             <div class="post-category d-flex flex-column mb-4">
                 <label class="mr-3" for="category_id">Category</label>
                 <select name="category_id" id="category_id" value="{{ old("category_id") }}">
@@ -41,7 +47,7 @@
                     @endforeach
                 </select>
             </div>
-
+            
             <button type="submit" class="btn btn-primary my-4">Submit post</button>
         </form>
     </div>
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -8,30 +8,36 @@
 
         @include("partials.errors")
 
+        <!-- Form for editing a post -->
         <form class="d-flex flex-column" action="{{ route("admin.posts.update", $post->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method("PUT")
         
+            <!-- Post title -->
             <div class="post-title d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Title</label>
                 <input type="text" name="title" id="title" value="{{ $post->title }}">
             </div>
 
+            <!-- Post image -->
             <div class="post-image d-flex flex-column mb-4">
                 <label class="mr-3" for="image">Image</label>
                 <input type="file" name="image" id="image">
             </div>
 
+            <!-- Post body -->
             <div class="post-body d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Body</label>
                 <input type="text" name="body" id="body" value="{{ $post->body }}">
             </div>
 
+            <!-- Post author -->
             <div class="post-author d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Author</label>
                 <input type="text" name="author" id="author" value="{{ $post->author }}">
             </div>
 
+            <!-- Post category -->
             <div class="post-category d-flex flex-column mb-4">
                 <label class="mr-3" for="category_id">Category</label>
                 <select name="category_id" id="category_id">
