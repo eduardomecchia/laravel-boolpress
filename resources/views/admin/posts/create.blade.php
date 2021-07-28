7d5bb486e4dd4b3fda79ae01df618ba17234ab8a
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-28 15:11:20 +0200

    Delete unused classes in create form and edit form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -13,31 +13,31 @@
             @csrf
         
             <!-- Post title -->
-            <div class="post-title d-flex flex-column mb-4">
+            <div class="d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Title</label>
                 <input type="text" name="title" id="title" value="{{ old("title") }}">
             </div>
 
             <!-- Post image -->
-            <div class="post-image d-flex flex-column mb-4">
+            <div class="d-flex flex-column mb-4">
                 <label class="mr-3" for="image">Image</label>
                 <input type="file" name="image" id="image">
             </div>
 
             <!-- Post body -->
-            <div class="post-body d-flex flex-column mb-4">
+            <div class="d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Body</label>
                 <input type="text" name="body" id="body" value="{{ old("body") }}">
             </div>
 
             <!-- Post author -->
-            <div class="post-author d-flex flex-column mb-4">
+            <div class="d-flex flex-column mb-4">
                 <label class="mr-3" for="title">Author</label>
                 <input type="text" name="author" id="author" value="{{ old("author") }}">
             </div>
 
             <!-- Post category -->
-            <div class="post-category d-flex flex-column mb-4">
+            <div class="d-flex flex-column mb-4">
                 <label class="mr-3" for="category_id">Category</label>
                 <select name="category_id" id="category_id" value="{{ old("category_id") }}">
                     <option disabled selected>Select a category</option>
@@ -47,7 +47,7 @@
                     @endforeach
                 </select>
             </div>
-            
+
             <button type="submit" class="btn btn-primary my-4">Submit post</button>
         </form>
     </div>
