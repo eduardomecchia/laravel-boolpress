00296627c84f7bafac45c2aeca136c79910e65fc
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-28 14:59:41 +0200

    Add tags method to Post model

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Tag;
 
 class Post extends Model
 {
@@ -16,5 +17,13 @@ class Post extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * The tags that belong to the post.
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
