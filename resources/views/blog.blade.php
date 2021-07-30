8b3a231acac3aef4c61aa769e485e8069f718386
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-30 15:40:15 +0200

    Print the posts in blog view using Vue

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,5 +3,13 @@
 @section('content')
     <div class="container" id="app">
         <h1>Blog</h1>
+
+        <div class="card text-left" v-for="post in posts">
+            <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">@{{post.title}}</h4>
+                <p class="card-text">@{{post.body}}</p>
+            </div>
+        </div>
     </div>
 @endsection
