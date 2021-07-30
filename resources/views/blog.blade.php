09a90ff705545324a83a60d7c2cb3917f36877fc
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-07-30 16:24:07 +0200

    Add category to post cards

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -9,6 +9,7 @@
                 <img class="img-fluid rounded" :src="post.image ? 'storage/' + post.image : '/img/placeholder.png'" width="200" alt="">
                 <div class="card-body p-0 mt-4">
                     <h4 class="card-title">@{{ post.title }}</h4>
+                    <div>@{{ post.category ? post.category.name : "No category available" }}</div>
                     <span>@{{ post.author }}</span>
                     <p class="card-text">@{{ post.body }}</p>
                 </div>
