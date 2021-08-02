fb3bafdb8f1360b23f4304b60e9ed8c64ba1099c
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-08-02 17:12:58 +0200

    Add return statement that flashes API token once after redirecting

--- app/Http/Controllers/Admin/ApiTokenController.php
+++ app/Http/Controllers/Admin/ApiTokenController.php
@@ -11,9 +11,11 @@ class ApiTokenController extends Controller
     public function update(Request $request)
     {
         $token = Str::random(80);
-        
+
         $request->user()->forceFill([
             'api_token' => hash('sha256', $token)
         ])->save();
+
+        return back()->with('api_token', $token);
     }
 }
