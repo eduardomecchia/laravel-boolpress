ba540f26e1431a61f897ffc6a35c1e85d7d3113c
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-08-02 17:10:05 +0200

    Add update method and Str import statement to new controller

--- app/Http/Controllers/Admin/ApiTokenController.php
+++ app/Http/Controllers/Admin/ApiTokenController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ApiTokenController extends Controller
 {
-    //
+    public function update(Request $request)
+    {
+        $token = Str::random(80);
+        dd($token);
+    }
 }
